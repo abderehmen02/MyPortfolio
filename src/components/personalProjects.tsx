da65870d5ee72012e0068bbd292d7242097c6883
abderrahmane <ilahalaha0987@gmail.com>
2023-07-03 13:31:27 +0100

    completing personal projects

--- src/components/personalProjects.tsx
+++ src/components/personalProjects.tsx
@@ -1,39 +1,52 @@
 "use client"
 
 import { personalProjectType, personalProjects } from "@/constants/personalProjects"
-import { Text } from "@/ui/typography"
+import { cn } from "@/lib/utils"
+import { BlodText, Text } from "@/ui/typography"
 import { useState } from "react"
 
 export const PersonalProjects : React.FC  = ()=>{
 const [CurrentProject, setCurrentProject] = useState<number>(0)
 const project : personalProjectType = personalProjects[CurrentProject]
 return <div className="flex items-center px-10 gap-5 py-4 relative bg-black text-white w-full">
-<div className="flex flex-col border-2  w-2/3" >
-    <div className="flex" >
-    <img  src={project.logo} />
-    <Text>{project.name}</Text>
+<div className="flex personalProjectContainer flex-col gap-8 py-5 cursor-cell bg-gray-900 rounded-2xl border-2  w-2/3" >
+    <div className="flex px-3 py-3 justify-around items-center" >
+    <div className="flex  gap-6 text-2xl " >
+    <i className="bi bi-github hover:text-secondaryDark cursor-pointer"></i>
+    <i className="bi bi-box-arrow-up-right hover:text-secondaryDark cursor-pointer"></i> 
     </div>
-    <Text>{project.ShortDescreption}</Text>
-    <div>
-    <Text>what i have learned</Text>
-    <ul>
-        {project.whatIHaveLearned.map((item , index)=><div key={index} >{item}</div>)}
+    <div className="flex items-center flex-col" >
+    <BlodText className="text-4xl text-primaryLight" >{project.name}</BlodText>
+    <Text className="text-xl capitalize text-whiteYellwed" ><i>{project.ShortDescreption}</i></Text>
+    </div>
+    <img  src={project.logo} className="w-28 h-28 rounded-full" />
+    </div>
+    <div className="flex gap-3 px-4" >
+    <div className="w-2/3 flex pb-6 gap-6 flex-col" >
+    <Text className="text-xl longDescriptionProject " >{project.LongDescreption}</Text>
+
+    <div className="gap-3 flex px-4 flex-col" >
+    <Text className="text-whiteYellwed text-3xl capitalize" >what i have learned</Text>
+    <ul className="list-disc " >
+        {project.whatIHaveLearned.map((item , index)=><li key={index} >{item}</li>)}
     </ul></div>
-    <div>
-    <Text>Technologies</Text>
-    <ul>
-        {project.technologies.map(technology=><li key={technology} >{technology}</li>)}
+
+    </div>
+    <div style={{ borderLeftWidth : '0.5px' }}  className="w-1/3 flex border-whiteYellwed items-center justify-center flex-col gap-7 shadow-lg " >
+    <BlodText className="text-whiteYellwed text-3xl" >Technologies</BlodText>
+    <ul className="text-lg    list-disc ps-5" >
+        {project.technologies.map(technology=><li key={technology} className="capitalize" >{technology}</li>)}
     </ul></div>
-    <Text>{project.LongDescreption}</Text>
+
+    </div>
     </div>
     <div className="w-1/3 flex gap-10   justify-center   flex-col items-center" >
-        <Text className="text-3xl text-whiteYellwed uppercase text-center" >Projects</Text>
-
-
-        <ul className=" flex  flex-col gap-4 px-10 py-6 rounded-xl projectsList w-fit " >
-            {personalProjects.map(project=><li className="text-xl hover:text-secondaryDark cursor-pointer  text-start" > <img className="w-7 h-7 inline rounded-full" src={project.logo} /> {project.name}  </li>)}
-        </ul>
+            <Text className="text-3xl text-whiteYellwed uppercase text-center" >Projects</Text>
+            <ul className=" flex  px-4 flex-col  gap-4  py-6 rounded-xl projectsList w-fit " >
+            {personalProjects.map( ( project , index )=><li onClick={()=>setCurrentProject(index)} className={cn(["text-xl   cursor-pointer  text-start"  , {"text-secondaryLight" : personalProjects[CurrentProject].name === project.name  } , { "hover:text-secondaryDark " : personalProjects[CurrentProject].name !== project.name } ])} > <img style={{border: '0.25px solid #fff'}} className="w-7 mx-3 h-7 inline rounded-full" src={project.logo} />    {project.name}  </li>)}
+            </ul>
     </div></div>
-
 }
 
+
+// "text-xl hover:text-secondaryDark cursor-pointer  text-start" 
--- src/constants/personalProjects.ts
+++ src/constants/personalProjects.ts
@@ -13,10 +13,10 @@ export interface personalProjectType {
 export const personalProjects : personalProjectType[] =[{
     name : 'Day After Day' ,
     github  : "https://github.com/abderehmen02/Day-After-Day" ,
-    whatIHaveLearned : ["building large complex web apps with typescript ,mern , redux " , "contributing to open source project and use git to manage app versions and branches" , "building great user interfaces with mui  and figma " , "implementing design patterns to make my code clean and manageble"   ] ,
+    whatIHaveLearned : ["building large complex web apps with typescript ,mern , redux " , "contributing to open source projects and use git to manage app versions and branches" , "building great user interfaces with mui  and figma " , "implementing design patterns to make my code clean and manageble"  , "ussing looks such as typescript , design patterns to make my code clean and optmizble in the future" , "implementing authorization technologies such as jwt to authonticate and authorize user" ] ,
     link : "https://marvelous-pie-441c92.netlify.app/"  ,
     ShortDescreption : " Productivity and day management app"    ,
-    LongDescreption : "an app to manage your toughts , set your goals ,<br/>write your dailly journalings and toughts and <br/>how many hours you have been productive <br/>each day    " ,
+    LongDescreption : "An app to manage your toughts , set your goals ,<br/>write your dailly journalings and toughts and <br/>how many hours you have been productive <br/>each day    " ,
     logo : '/dayAfterDayLogo.png' ,
     technologies : [ "mern stack" , "typescript"    , "redux"  , "design patterns"  , "git"  , "material ui"  , "figma"   ]
 } , {
@@ -24,7 +24,7 @@ export const personalProjects : personalProjectType[] =[{
     logo : "/ChatXLogo.ico" ,
     github  : "https://github.com/abderehmen02/Chat-X"  ,
     link : "https://stellular-genie-c77a7c.netlify.app/" ,
-    whatIHaveLearned : ["building complex web app with firebase as a database"  , "working with material ui and figma to build an outstanding ui and ux"  ] ,
+    whatIHaveLearned : ["building complex web app ussing firebase as a database"  , "working with material ui and figma to build an outstanding ui and ux"  ] ,
     ShortDescreption :" a chat app similar to instagram"  ,
     LongDescreption : "a social media app where you can see your friend s posts , write comments , like posts , search for members and follow other people    "  ,
     technologies : ["firebase" , "react js" , "material ui  "  , "material ui"  , "figma" ] ,
