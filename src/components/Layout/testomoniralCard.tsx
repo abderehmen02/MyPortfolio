a3087b58b74ca6f821d7e17c017dd1447db43ef2
abderrahmane <ilahalaha0987@gmail.com>
2023-07-04 10:56:55 +0100

    fixing testomonial width

--- src/components/Layout/testomoniralCard.tsx
+++ src/components/Layout/testomoniralCard.tsx
@@ -3,7 +3,7 @@ import { Text } from "@/ui/typography"
 import React from "react"
 
 export const TestomonialCard : React.FC<TestomonialType> =  ({email , fullname , message ,phone_number})=>{
-return <div style={{width : "400px"}} className="flex flex-col items-center px-4 py-9  bg-black border-2 rounded-xl  border-whiteYellwed gap-6" >
+return <div  className="flex max-w-xl testomonialCard flex-col items-center px-4 py-9  bg-black border-2 rounded-xl laptop:w-1/3 border-whiteYellwed gap-6" >
 <Text className="text-4xl  capitalize w-wit text-whiteYellwed " >{fullname}</Text>
 <Text className="text-lg text-white w-fit text-center" >{message}</Text>
 <div className="flex flex-col" >
