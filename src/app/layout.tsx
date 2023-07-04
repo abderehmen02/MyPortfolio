8b93e6e17aa4d271f80169fdc525833ca9f13e47
abderrahmane <ilahalaha0987@gmail.com>
2023-07-04 11:24:37 +0100

    fixing ui in xl screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 import "bootstrap-icons/font/bootstrap-icons.css"
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -16,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body ><div className='appContainer' >{children}</div></body>
     </html>
   )
 }
--- src/components/certaficates.tsx
+++ src/components/certaficates.tsx
@@ -3,7 +3,7 @@ import { CertaficateCard } from "./Layout/certaficateCard"
 import { BlodText } from "@/ui/typography"
 
 export const CertaficateSection : React.FC = ()=>{
-    return <div className="flex flex-col bg-black text-white py-6 items-center justify-center" >
+    return <div className="flex flex-col bg-black w-full text-white py-6 items-center justify-center" >
         <BlodText className="text-primaryLight my-12 w-fit text-5xl" >Certaficates</BlodText>
     <div  className="flex w-full flex-wrap items-center justify-center bg-black text-white">
         {certaficates.map((certaficate) =><CertaficateCard {...certaficate} />)}
