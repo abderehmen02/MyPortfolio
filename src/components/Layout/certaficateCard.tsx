716b373ead81e2cabebce5d6c1b7e928c0e319b0
abderrahmane <ilahalaha0987@gmail.com>
2023-07-04 10:51:16 +0100

    setting certaficate card for mobile

--- src/components/Layout/certaficateCard.tsx
+++ src/components/Layout/certaficateCard.tsx
@@ -3,7 +3,7 @@ import { Text } from "@/ui/typography"
 import { Oranienbaum } from "next/font/google"
 
 export const CertaficateCard : React.FC<certaficateType> = ({organization , url , companyLogo })=>{
-    return <div className="flex flex-col rounded-xl  w-9/12 relative  border-2" >
+    return <div className="flex flex-col rounded-xl w-full  laptop:w-9/12 relative  border-2" >
 <img  src={url} className=" w-full rounded-t-xl" />
 <div className="px-6 py-6 flex items-center justify-center gap-5 " >
 <Text className="w-full font-semibold text-3xl text-center">{organization}</Text>
